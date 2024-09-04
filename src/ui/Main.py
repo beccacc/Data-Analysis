29e1335b25922537a5f89f179a461535461db03f
beccacc <rebecca.chucarroll@gmail.com>
2024-09-04 16:25:54 -0700

    Added chooseUsage to allow querying TODO: - displayResults for querying - test all operations - add option to "explain results"(in DisplayResults) - improve DisplayResults ui appearance

--- src/ui/Main.py
+++ src/ui/Main.py
@@ -1,8 +1,8 @@
 from FileUpload import FileUpload
-from SelectFiles import SelectFiles
-from Operations import Operations
-from ChooseVariables import ChooseVariables
-from DisplayResults import DisplayResults
+from SelectFiles import SelectFiles, SelectFile
+from Operations import MultiFileOperations, SingleFileOperations
+from ChooseVariables import ChooseVariables, ChooseVariable
+from DisplayResults import DisplayResultsMulti, DisplayResultsOne
 from ChooseUsage import ChooseUsage
 import sys
 import os
@@ -14,13 +14,23 @@ from model.PerformOperation import PerformOperation
 
 def main():
     fileUpload = FileUpload()
-    selectFiles = SelectFiles(fileUpload)
+    chooseUsage = ChooseUsage()
+    if(chooseUsage.getUsage()=="multiFile"):
+        selectFiles = SelectFiles(fileUpload)
+        operation = MultiFileOperations()
+        chooseVariables = ChooseVariables(selectFiles, operation)
+        performOperation = PerformOperation(chooseVariables, operation)
+        displayResults = DisplayResultsMulti(performOperation)
+        displayResults.display()
+    else:
+        selectFile = SelectFile(fileUpload)
+        chooseVariable = ChooseVariable(selectFile)
+        operation = SingleFileOperations(chooseVariable, selectFile)
+        displayResults = DisplayResultsOne(operation)
+
+
     # chooseUsage = ChooseUsage()
-    operation = Operations()
-    chooseVariables = ChooseVariables(selectFiles, operation)
-    performOperation = PerformOperation(chooseVariables, operation)
-    displayResults = DisplayResults(performOperation)
-    displayResults.display()
+    
  
     # chooseVariables.chooseVars()
     
